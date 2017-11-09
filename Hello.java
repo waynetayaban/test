497c705391c595223bf9dc7743add2aece665fa0
WAYNE <jvwtayaban@gmail.com>
2017-11-09 20:08:19 +0800

    Third commit

--- Hello.java
+++ Hello.java
@@ -2,5 +2,6 @@ public class Hello {
 	public static void main (String [] args) {
 		System.out.println("Hello Wayne! ");
 		Sytem.out.println("Changes are made here");
+		System.out.println("Hey this is the Remote repo scene");
 	}
 }
